07be3b12a5678f43b4defc4db80815906da3d8f2
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-09 18:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ function whisper(string) {return string.toLowerCase() }
 function logShout(string) { console.log('HELLO') } 
 function logWhisper(string) { console.log('hello') }
 function sayHiToGrandma(string) { return string.toUpperCase() }
+function sayHiToGrandma(string) {console.log('I LOVE YOU, GRANDMA.') return string('I love you, too.')}
