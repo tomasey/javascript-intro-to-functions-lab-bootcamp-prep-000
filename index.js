abf8d5b5e8d0d8c07cf09355a5f9a2b120d8b964
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-09 17:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function shout(string) {return string.toUpperCase() }
 function whisper(string) {return string.toLowerCase() }
 function logShout(string) { console.log('HELLO') } 
-function logWhisper(string) { console.log('')
+function logWhisper(string) { console.log('hello') }
